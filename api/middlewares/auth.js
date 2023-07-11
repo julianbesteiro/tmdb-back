6f6c4df52d5bd4168293bfe9ff7ae4cc48d76304
julianbesteiro <julianbesteiro1@gmail.com>
2023-07-11 19:59:52 -0300

    fix: test secret route

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -4,6 +4,7 @@ function validateAuth(req, res, next) {
   //const token = req.cookies.token;
 
   const token = req.body.token;
+  console.log("token", token);
   if (!token) return res.sendStatus(401);
 
   const { user } = validateToken(token);
